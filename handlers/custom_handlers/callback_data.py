faaae3a1bbce8ff9f36e332a62e363232b134edd
spiderman-1500 <138768234+spiderman-1500@users.noreply.github.com>
2023-11-19 19:58:44 +0600

    tg bot

--- handlers/custom_handlers/callback_data.py
+++ handlers/custom_handlers/callback_data.py
@@ -1,12 +1,11 @@
 from loader import bot
 from states.contact_information import UserInfoState
-from handlers.custom_handlers.city_handler import handle_city
+from utils.city_handler import handle_city
 import re
 
 
-@bot.callback_query_handler(func=lambda call: call.data.startswith("callback_data:"), state=UserInfoState.callback_data)
+@bot.callback_query_handler(func=lambda call: call.data.startswith("callback_data:"))
 def handle_location_callback(call):
-    chosen_city = re.search(r'location:(.*)', call.data).group(1) # Получение выбранного города из данных обратного вызова
+    chosen_city = re.search(r'callback_data:(.*)', call.data).group(1) # Получение выбранного города из данных обратного вызова
     handle_city(call.message)
     bot.send_message(call.message.chat.id, f'Вы выбрали город: {chosen_city}')
-
