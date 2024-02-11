8a30fdd3952782ec3aeb8158179dc7381d4e02d5
spiderman-1500 <138768234+spiderman-1500@users.noreply.github.com>
2024-02-11 12:13:40 +0600

    tg bot

--- handlers/custom_handlers/children.py
+++ handlers/custom_handlers/children.py
@@ -5,13 +5,11 @@ from telebot.types import Message
 
 @bot.message_handler(state=UserInfoState.amount_child)
 def child(message: Message) -> None:
-    if message.text.lower() == 'да':
+    if message.text.lower() == 'нет':
+        return UserInfoState.date_entry
+    elif message.text.lower() == 'да':
         bot.send_message(message.chat.id, 'Кол-во детей')
-        bot.set_state(message.from_user.id, UserInfoState.age_child, message.chat.id)
-    elif message.text.lower() == 'нет':
-        bot.send_message(message.chat.id, 'Хорошо, детей с собой нету')
-        bot.send_message(message.chat.id, 'Введите дату въезда в формате (01.04.2021)')
-        bot.set_state(message.from_user.id, UserInfoState.date_entry, message.chat.id)
+        bot.state(message.from_user.id, UserInfoState.age_child, message.chat.id)
 
 
 @bot.message_handler(state=UserInfoState.age_child)
--- handlers/custom_handlers/entry_data.py
+++ handlers/custom_handlers/entry_data.py
@@ -1,17 +1,21 @@
 from loader import bot
 from states.contact_information import UserInfoState
 from telebot.types import Message
-from datetime import datetime,date
 
 
 @bot.message_handler(state=UserInfoState.date_entry)
 def entry_date(message: Message) -> None:
-    if message.text.isdigit():
+    if message.text.lower() == 'нет':
+        bot.send_message(message.chat.id, 'Хорошо, детей с собой нету, введите дату въезда в формате (01.04.2021)')
+        bot.set_state(message.from_user.id, UserInfoState.date_exit, message.chat.id)
+
+    elif message.text.isdigit():
         with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
             data['child_age'] = message.text
         bot.set_state(message.from_user.id, UserInfoState.date_exit, message.chat.id)
         bot.send_message(message.from_user.id, 'Возраст записан. Теперь введите дата '
                                                'въезда в таком формате (01.04.2021)')
+
     else:
         bot.send_message(message.from_user.id, 'Возраст может быть только числом')
 
