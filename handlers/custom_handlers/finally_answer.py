ff609cfe94a9f8b8fc22cf3084e49a5b68fc7b34
spiderman-1500 <138768234+spiderman-1500@users.noreply.github.com>
2024-05-01 22:22:34 +0600

    tg bot

--- handlers/custom_handlers/finally_answer.py
+++
@@ -1,9 +0,0 @@
-# from utils.hotels_type import get_data
-# from loader import bot
-# from telebot.types import Message
-#
-#
-# @bot.send_message
-# def answer(message: Message, **kwargs)->get_data:
-#     result = get_data(**kwargs)
-#     bot.send_message(message.chat.id, result)
