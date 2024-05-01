36d8a5e650a91117ca402305b6fb6233addbe600
spiderman-1500 <138768234+spiderman-1500@users.noreply.github.com>
2024-05-01 22:21:45 +0600

    tg bot

--- main.py
+++ main.py
@@ -8,17 +8,4 @@ if __name__ == '__main__':
     bot.infinity_polling()
 
 
-# def test():
-#     a = input('Enter ages with commas: ')
-#     ages = [int(age.strip()) for age in a.split(',') if age.strip().isdigit()]
-#     if ages:
-#         for age in ages:
-#             print(age, end=' ')
-#             print(type(age), end=' ')
-#     else:
-#         print('Error')
-#
-# test()
-#
-#
 
